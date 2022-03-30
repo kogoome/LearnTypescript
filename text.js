5a1451e621bda72acb64c88e704bb27ddd3bf37a
chanwon moon <kogoome@kakao.com>
2022-03-30 17:57:36 +0900

    테스트

--- text.js
+++ text.js
@@ -1,18 +1,23 @@
 const Obj2 = {
   description: "a",
+  hashName: "@obj2",
   relation: {
-    superset: ["1"],
+    superset: ["@Obj1"],
   }
 };
 
 const Obj1 = {
   description: "A",
+  hashName: "@Obj1",
   relation: {
-    subset: [Obj2],
+    subset: ["@Obj2"],
   }
 };
 
-
+const hashLinkTable = {
+  "@Obj1": Obj1,
+  "@Obj2": Obj2
+}
 
 console.log(Obj1);
 // function isIncluding(a, A) {
