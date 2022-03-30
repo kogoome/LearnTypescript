102bbb1f29e3de25af4a2bbaedaac395d23513db
kogoome <kogoome@kakao.com>
2022-03-30 19:54:14 +0900

    텍스트

--- text.js
+++ text.js
@@ -1,25 +1,49 @@
-const Obj2 = {
-  description: "a",
-  hashName: "@obj2",
-  relation: {
-    superset: ["@Obj1"],
-  }
-};
+const color = {
+  node: "color",
+  hash: "@color",
 
-const Obj1 = {
-  description: "A",
-  hashName: "@Obj1",
-  relation: {
-    subset: ["@Obj2"],
-  }
-};
-
-const hashLinkTable = {
-  "@Obj1": Obj1,
-  "@Obj2": Obj2
 }
 
-console.log(Obj1);
-// function isIncluding(a, A) {
-//   return A.a == null ? "no" : "yes"
-// }
+const apple = {
+  node: "apple",
+  hash: "@apple",
+  edge: {
+    subset:[],
+    superset: ["@fruits"],
+    color: ["@red", "@green"],
+  }
+};
+
+const fruit = {
+  node: "fruit",
+  hash: "@fruits",
+  color: "not defined",
+  edge: {
+    subset: ["@apple"],
+    superset:[],
+  }
+};
+
+const hashTable = {
+
+}
+
+
+
+function isSubset(subset, superset) {
+  return superset.edge.subset.filter(obj=>obj == subset.hash ) ?
+    `${subset.node} is ${superset.node}` :
+    `${subset.node} is not ${superset.node}`
+}
+
+console.log(isSubset(apple, fruit));
+
+function strHashing (str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash += str.charCodeAt(i) * i;
+  }
+  return hash;
+}
+
+console.log(strHashing("apple"));
