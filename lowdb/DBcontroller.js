7b4570e3cf1e0e7a4e08697945ea9a67b36ff8ab
kogoome <kogoome@kakao.com>
2022-04-03 09:08:50 +0900

    비크립트

--- lowdb/DBcontroller.js
+++ lowdb/DBcontroller.js
@@ -1,8 +1,7 @@
 import { join, dirname } from 'path'
 import { Low, JSONFile } from 'lowdb'
 import { fileURLToPath } from 'url'
-import { strHashing } from './hashing';
-
+import bcrypt from 'bcrypt'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, 'propositionDB.json')
@@ -12,28 +11,8 @@ const db = new Low(adapter)
 // Read data from JSON file, this will set db.data content
 await db.read()
 const Data = db.data
-Data = [
-  "hashTable" = {},
-  "ConceptTable" = {},
-  "Concept" = {},
-  "hashPath" = {
-    subset: [],
-    include: []
-  }
-]
-
-Data.hashTable = {
-  619: tree,
-  633: food,
-  1063: apple,
-  1127: fruit,
-}
-Data.Concept = {}
-
 
 console.log(Data);
-
-
 
 
 
